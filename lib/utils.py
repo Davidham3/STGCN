76e738a7dfcefa6ba8a3f8160915fb49bfb0b533
Davidham3 <chaosong@bjtu.edu.cn>
2019-06-04 02:09:36 -0400

    update

--- lib/utils.py
+++ lib/utils.py
@@ -1,91 +1,288 @@
 # -*- coding:utf-8 -*-
+# pylint: disable=no-member
+import time
+import csv
+import json
 import numpy as np
 from scipy.sparse.linalg import eigs
+from mxnet import autograd
+from mxnet import nd
+from mxboard import SummaryWriter
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+
 
 def scaled_Laplacian(W):
     '''
     compute \tilde{L}
-    
+
     Parameters
     ----------
     W: np.ndarray, shape is (N, N), N is the num of vertices
-    
+
     Returns
     ----------
     scaled_Laplacian: np.ndarray, shape (N, N)
-    
+
     '''
-    
+
     assert W.shape[0] == W.shape[1]
-    
-    D = np.diag(np.sum(W, axis = 1))
-    
+
+    D = np.diag(np.sum(W, axis=1))
+
     L = D - W
-    
-    lambda_max = eigs(L, k = 1, which = 'LR')[0].real
-    
+
+    lambda_max = eigs(L, k=1, which='LR')[0].real
+
     return (2 * L) / lambda_max - np.identity(W.shape[0])
+
 
 def cheb_polynomial(L_tilde, K):
     '''
     compute a list of chebyshev polynomials from T_0 to T_{K-1}
-    
+
     Parameters
     ----------
     L_tilde: scaled Laplacian, np.ndarray, shape (N, N)
-    
+
     K: the maximum order of chebyshev polynomials
-    
+
     Returns
     ----------
     cheb_polynomials: list(np.ndarray), length: K, from T_0 to T_{K-1}
-    
+
     '''
-    
+
     N = L_tilde.shape[0]
-    
+
     cheb_polynomials = [np.identity(N), L_tilde.copy()]
-    
+
     for i in range(2, K):
-        cheb_polynomials.append(2 * L_tilde * cheb_polynomials[i - 1] - cheb_polynomials[i - 2])
-        
+        cheb_polynomials.append(2 * L_tilde * cheb_polynomials[i - 1] -
+                                cheb_polynomials[i - 2])
+
     return cheb_polynomials
 
-def get_adjacency_matrix(distance_df, num_of_vertices, normalized_k=0.1):
-    """
+
+def get_adj_matrix(adj_filename, num_of_vertices, sigma2=0.1, epsilon=0.5):
+    '''
     Parameters
     ----------
-    distance_df: pd.DataFrame, contains distance between vertices, three columns [from, to, distance]
+    adj_filename: str, path of adj file
 
-    num_of_vertices: int, number of vertices
+    num_of_vertices, int, number of vertices
 
-    normalized_k: parameter of gaussian kernel
-    
+    sigma2, epsilon: float
+
     Returns
     ----------
     A: np.ndarray, adjacency matrix
 
-    """
-    A = np.zeros((num_of_vertices, num_of_vertices), dtype = np.float32)
+    indices_dict: dict, map station_id into integers from 0 to num_of_vertices
 
-    # Fills cells in the matrix with distances.
-    for row in distance_df.values:
-        i, j = int(row[0]), int(row[1])
-        A[i, j] = row[2]
-        A[j, i] = row[2]
+    '''
+    A = np.zeros(shape=(num_of_vertices, num_of_vertices))
+    indices_dict = {}
 
-    # Calculates the standard deviation as theta.
-    # compute the variance of the all distances which does not equal zero
-    mask = (A == 0)
+    def transform_indices(id_):
+        if id_ not in indices_dict:
+            indices_dict[id_] = len(indices_dict)
 
-    tmp = A.flatten()
-    var = np.var(tmp[tmp!=0])
+    with open(adj_filename, 'r') as f:
+        csv_reader = csv.reader(f)
+        _ = csv_reader.__next__()
+        for start, end, distance in csv_reader:
+            start, end = int(start), int(end)
+            transform_indices(start)
+            transform_indices(end)
+            A[indices_dict[start], indices_dict[end]] = float(distance)
+
+    assert len(indices_dict) == num_of_vertices
+
+    mask = (np.ones((num_of_vertices, num_of_vertices)) -
+            np.identity(num_of_vertices))
 
     # normalization
-    A = np.exp(- (A ** 2) / var)
+    A = np.exp(- (A ** 2) / sigma2)
+    A = A * (A >= epsilon) * mask
 
-    # drop the value less than threshold
-    A[A < normalized_k] = 0
-    A[mask] = 0
-    
-    return A
+    return A, indices_dict
+
+
+def data_preprocess(indices_dict, graph_signal_filename):
+    '''
+    Parameters
+    ----------
+    indices_dict: dict
+
+    graph_signal_filename: str
+
+    Returns
+    ----------
+    X: Graph signal matrix, np.ndarray, shape is
+        (num_of_vertices, num_of_features, num_of_samples)
+    '''
+
+    # preprocessing graph signal data
+    with open(graph_signal_filename, 'r') as f:
+        data = json.loads(f.read().strip())
+
+    random_key = str(list(indices_dict)[0])
+    random_feature_name = list(data[random_key].keys())[0]
+    num_of_vertices = len(indices_dict)
+
+    # initialize the graph signal matrix
+    # shape is (num_of_vertices, num_of_features, num_of_samples)
+    X = np.empty(shape=(num_of_vertices,
+                        len(data[random_key].keys()),
+                        len(data[random_key][random_feature_name])))
+
+    # idx is the index of the vertice
+    for vertice, idx in indices_dict.items():
+        vertice = str(vertice)
+        X[idx, 0, :] = np.array(data[vertice]['flow'])
+        X[idx, 1, :] = np.array(data[vertice]['occupy'])
+        X[idx, 2, :] = np.array(data[vertice]['speed'])
+
+    return X[:, 0: 1, :]
+
+
+def build_dataset(graph_signal_matrix, num_points_for_train,
+                  num_points_for_predict):
+    '''
+    Parameters
+    ----------
+    graph_signal_matrix: np.ndarray, graph signal matrix, shape is
+        (num_of_vertices, num_of_features, num_of_samples)
+
+    Returns
+    ----------
+    features: np.ndarray, shape is
+        (num_of_samples, num_of_features,
+         num_of_vertices, num_points_for_training)
+
+    target: np.ndarray, shape is
+        (num_of_samples, num_of_vertices, num_points_for_prediction)
+    '''
+
+    # generate the beginning index and the ending index of a sample,
+    # which bounds (num_points_for_training + num_points_for_predicting) points
+    indices = [(i, i + (num_points_for_train + num_points_for_predict))
+               for i in range(
+                   graph_signal_matrix.shape[2] -
+                   (num_points_for_train + num_points_for_predict) + 1)]
+
+    # save samples
+    features, target = [], []
+    for i, j in indices:
+        features.append(
+            graph_signal_matrix[:, :, i: i + num_points_for_train]
+                .transpose((1, 0, 2)))
+        target.append(graph_signal_matrix[:, 0, i + num_points_for_train: j])
+
+    features = np.concatenate([np.expand_dims(i, 0) for i in features], axis=0)
+    target = np.concatenate([np.expand_dims(i, 0) for i in target], axis=0)
+
+    return features, target
+
+
+def predict(net, x, num_points_for_train, num_points_for_predict):
+    '''
+    net
+
+    x: mx.ndarray, shape is
+        (batch_size, num_of_features, num_of_vertices, num_of_training_point)
+
+    num_points_for_train: int
+
+    num_points_for_predict: int
+
+    Returns
+    ----------
+    mx.ndarray, shape is
+        (batch_size, num_of_vertices, num_of_training_point)
+    '''
+    predictions = []
+    for _ in range(num_points_for_predict):
+        # pylint: disable=invalid-sequence-index
+        p = nd.concat(x, *predictions, dim=-1)[:, :, :, -num_points_for_train:]
+        predictions.append(net(p).expand_dims(1))
+    return nd.concat(*predictions, dim=-1).squeeze(axis=1)
+
+
+def train_model(net, sw, train_count, val_count,
+                training_dataloader, validation_dataloader,
+                testing_dataloader, epochs, loss_function,
+                trainer, decay_interval=None, decay_rate=None):
+    '''
+    train the model
+
+    Parameters
+    ----------
+    net: model which has been initialized
+
+    sw: mxboard.SummarayWriter
+
+    training_dataloader: gluon.data.dataloader.DataLoader
+    validation_dataloader: gluon.data.dataloader.DataLoader
+    testing_dataloader: gluon.data.dataloader.DataLoader
+
+    Returns
+    ----------
+    train_loss_list: list[float], which contains loss of training
+
+    val_loss_list: list[float], which contains loss of validation
+
+    test_loss_list: list[float], which contains loss of testing
+
+    '''
+    for epoch in range(epochs):
+
+        for x, y in training_dataloader:
+            with autograd.record():
+                l = loss_function(net(x), y)
+            l.backward()
+            trainer.step(x.shape[0])
+            sw.add_scalar('training_loss',
+                          value=l.mean().asscalar(),
+                          global_step=train_count)
+            train_count += 1
+
+        val_loss_list = [loss_function(net(x), y).mean().asscalar()
+                         for x, y in validation_dataloader]
+        sw.add_scalar('val_loss',
+                      value=sum(val_loss_list) / len(val_loss_list),
+                      global_step=val_count)
+
+        predictions, ground_truth = zip(*[(predict(net, x, 12, 12).asnumpy(),
+                                          y.asnumpy())
+                                          for x, y in testing_dataloader])
+        predictions = np.concatenate(predictions, axis=0)
+        ground_truth = np.concatenate(ground_truth, axis=0)
+        predictions = predictions.reshape(predictions.shape[0], -1)
+        ground_truth = ground_truth.reshape(ground_truth.shape[0], -1)
+
+        sw.add_scalar('mae',
+                      value=mean_absolute_error(ground_truth, predictions),
+                      global_step=val_count)
+        sw.add_scalar('rmse',
+                      value=mean_squared_error(ground_truth, predictions)**0.5,
+                      global_step=val_count)
+        val_count += 1
+
+        if (epoch + 1) % 5 == 0:
+            filename = 'stgcn_params/stgcn.params_{}'.format(epoch)
+            net.save_parameters(filename)
+
+        if decay_interval:
+            if (epoch + 1) % decay_interval == 0:
+                trainer.set_learning_rate(trainer.learning_rate * decay_rate)
+
+    return train_count, val_count
+
+if __name__ == "__main__":
+    adj_filename = 'data/test_data1/distance.csv'
+    graph_signal_filename = 'data/test_data1/graph_signal_data_small.txt'
+    A, indices_dict = get_adj_matrix(adj_filename, 307)
+    X = data_preprocess(indices_dict, graph_signal_filename)
+    print(A.sum(), X.sum())
--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 import json
 import time
 import os
+import shutil
+import configparser
+import argparse
 
 import numpy as np
-import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 import mxnet as mx
@@ -13,271 +15,179 @@ from mxnet import nd
 from mxnet import autograd
 from mxnet import gluon
 from mxnet.gluon import Trainer
+from mxboard import SummaryWriter
 
-from lib.utils import *
+from lib import utils
 from model import STGCN
 
 ##########
 # configuration part
 
-# choose which device you want to use, if you want to use GPU, set ctx = mx.gpu(0)
-ctx = mx.cpu()
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--config",
+    type=str,
+    help="configuration file path",
+    required=True
+)
+parser.add_argument(
+    "--logdir",
+    type=str,
+    help="tensorboard logdir",
+    required=True
+)
+parser.add_argument(
+    "--rm",
+    type=bool,
+    help="remove log dir if exists",
+    required=False
+)
+args = parser.parse_args()
 
-# number of vertices in your graph
-num_of_vertices = 307
+config = configparser.ConfigParser()
+print('read configuration file: {}'.format(args.config))
+config.read(args.config)
 
-# orders of chebyshev polynomials
-orders_of_cheb = 3
+data_configs = config['Data']
 
-# we use a Gaussian kernel to normalize the adjacency matrix, epsilon is the threshold
-epsilon = 0.5
+adj_filename = data_configs['adj_filename']
+graph_signal_filename = data_configs['graph_signal_filename']
+num_of_vertices = int(data_configs['num_of_vertices'])
+params_dir = data_configs['params_dir']
 
-# how many points you want to train the model and how many you want to predict
-num_points_for_train, num_points_for_predict = 12, 1
+model_configs = config['Model']
 
-# learning rate
-learning_rate = 1e-2
+orders_of_cheb = int(model_configs['orders_of_cheb'])
 
-# optimizer
-optimizer = 'RMSprop'
+ctx = model_configs['ctx']
+if ctx.startswith('cpu'):
+    ctx = mx.cpu()
+elif ctx.startswith('gpu'):
+    ctx = mx.gpu(int(ctx[ctx.find('-') + 1:]))
+else:
+    raise ValueError("context seems to be wrong!")
 
-# decay rate
-decay_rate = 0.7
+optimizer = model_configs['optimizer']
+learning_rate = float(model_configs['learning_rate'])
 
-# decay interval
-decay_interval = 5
+batch_size = model_configs['batch_size']
+epochs = int(model_configs['epochs'])
 
-# training epochs
-epochs = 10
+decay_interval = model_configs['decay_interval']
+if decay_interval == 'None':
+    decay_interval = None
+decay_rate = model_configs['decay_rate']
+if decay_rate == 'None':
+    decay_rate = None
 
-# batch_size
-batch_size = 32
 ##########
 
-def data_preprocess(adj_filename):
-    '''
-    Returns
-    ----------
-    A: Adjacency matrix, np.ndarray, shape is (num_of_vertices, num_of_vertices)
-    
-    X: Graph signal matrix, np.ndarray, shape is (num_of_vertices, num_of_features, num_of_samples)
-    '''
-    
-    # distance information between vertices in the graph
-    distance = pd.read_csv(adj_filename)
+A, indices_dict = utils.get_adj_matrix(adj_filename, num_of_vertices)
+X = utils.data_preprocess(indices_dict, graph_signal_filename)
 
-    # initialize an adjacency matrix of the graph with 0
-    A = np.zeros((num_of_vertices, num_of_vertices))
-    
-    # put distances into the corresponding position
-    for x, y, dis in distance.values:
-        x_index = int(x)
-        y_index = int(y)
-        A[x_index, y_index] = dis
-        A[y_index, x_index] = dis
+L_tilde = utils.scaled_Laplacian(A)
+cheb_polys = [nd.array(i, ctx=ctx)
+              for i in utils.cheb_polynomial(L_tilde, orders_of_cheb)]
 
-    # compute the variance of the all distances which does not equal zero
-    tmp = A.flatten()
-    var = np.var(tmp[tmp!=0])
+# training: validation: testing = 6: 2: 2
+split_line1 = int(X.shape[2] * 0.6)
+split_line2 = int(X.shape[2] * 0.8)
 
-    # normalization
-    A = np.exp(- (A ** 2) / var)
+train_original_data = X[:, :, : split_line1]
+val_original_data = X[:, :, split_line1: split_line2]
+test_original_data = X[:, :, split_line2:]
 
-    # drop the value less than threshold
-    A[A < epsilon] = 0
-    
-    # preprocessing graph signal data
-    with open('data/graph_signal_data_small.txt', 'r') as f:
-        data = json.loads(f.read().strip())
+training_data, training_target = utils.build_dataset(
+    train_original_data, 12, 1)
 
-    # initialize the graph signal matrix, shape is (num_of_vertices, num_of_features, num_of_samples)
-    X = np.empty(shape = (num_of_vertices,  # num_of_vertices
-                          len(data[list(data.keys())[0]].keys()),  # num_of_features
-                          len(list(data[list(data.keys())[0]].values())[0])),  # num_of_samples
-                )
-    
-    # i is the index of the vertice
-    for i in range(num_of_vertices):
-        X[i, 0, :] = np.array(data[str(i)]['flow'])
-        X[i, 1, :] = np.array(data[str(i)]['occupy'])
-        X[i, 2, :] = np.array(data[str(i)]['speed'])
-    return A, X
+val_data, val_target = utils.build_dataset(
+    val_original_data, 12, 1)
 
-def build_dataset(graph_signal_matrix):
-    '''
-    Parameters
-    ----------
-    graph_signal_matrix: np.ndarray, graph signal matrix, shape is (num_of_vertices, num_of_features, num_of_samples)
-    
-    Returns
-    ----------
-    features: np.ndarray, shape is (num_of_samples, num_of_features, num_of_vertices, num_points_for_training)
-    
-    target: np.ndarray, shape is (num_of_samples, num_of_vertices, num_points_for_prediction)
-    '''
-    
-    # generate the beginning index and the ending index of a sample, which bounds (num_points_for_training + num_points_for_predicting) points
-    indices = [(i, i + (num_points_for_train + num_points_for_predict)) 
-                for i in range(graph_signal_matrix.shape[2] - (num_points_for_train + num_points_for_predict) + 1)]
-    
-    # save samples
-    features, target = [], []
-    for i, j in indices:
-        features.append(graph_signal_matrix[:, :, i: i + num_points_for_train].transpose((1, 0, 2)))
-        target.append(graph_signal_matrix[:, 0, i + num_points_for_train: j])
-    
-    features = np.concatenate([np.expand_dims(i, 0) for i in features], axis=0)
-    target = np.concatenate([np.expand_dims(i, 0) for i in target], axis=0)
-    
-    return features, target
+testing_data, testing_target = utils.build_dataset(
+    test_original_data, 12, 12)
 
-def train_model(net, training_dataloader, validation_dataloader, testing_dataloader):
-    '''
-    train the model
-    
-    Parameters
-    ----------
-    net: model which has been initialized
-    
-    training_dataloader, validation_dataloader, testing_dataloader: gluon.data.dataloader.DataLoader
-    
-    Returns
-    ----------
-    train_loss_list: list(float), which contains loss values of training process
-    
-    val_loss_list: list(float), which contains loss values of validation process
-    
-    test_loss_list: list(float), which contains loss values of testing process
-    
-    '''
-    train_loss_list = []
-    val_loss_list = []
-    test_loss_list = []
-    for epoch in range(epochs):
-        t = time.time()
+# Z-score preprocessing
+assert num_of_vertices == training_data.shape[2]
+_, num_of_features, _, _ = training_data.shape
 
-        train_loss_list_tmp = []
-        for x, y in training_dataloader:
-            with autograd.record():
-                output = net(x)
-                l = loss_function(output, y)
-            l.backward()
-            train_loss_list_tmp.append(l.mean().asnumpy()[0])
-            trainer.step(batch_size)
+transformer = StandardScaler()
+training_data_norm = transformer.fit_transform(
+    training_data.reshape(training_data.shape[0], -1))\
+    .reshape(training_data.shape[0], num_of_features, num_of_vertices, 12)
+val_data_norm = transformer.transform(
+    val_data.reshape(val_data.shape[0], -1))\
+    .reshape(val_data.shape[0], num_of_features, num_of_vertices, 12)
+testing_data_norm = transformer.transform(
+    testing_data.reshape(testing_data.shape[0], -1))\
+    .reshape(testing_data.shape[0], num_of_features, num_of_vertices, 12)
 
-        train_loss_list.append( sum(train_loss_list_tmp) / len(train_loss_list_tmp) )
+training_data_norm = nd.array(training_data_norm, ctx=ctx)
+val_data_norm = nd.array(val_data_norm, ctx=ctx)
+testing_data_norm = nd.array(testing_data_norm, ctx=ctx)
 
-        val_loss_list_tmp = []
-        for x, y in validation_dataloader:
-            output = net(x)
-            val_loss_list_tmp.append(loss_function(output, y).mean().asnumpy()[0])
+training_target = nd.array(training_target, ctx=ctx)
+val_target = nd.array(val_target, ctx=ctx)
+testing_target = nd.array(testing_target, ctx=ctx)
 
-        val_loss_list.append( sum(val_loss_list_tmp) / len(val_loss_list_tmp) )
+print('training data shape:',
+      training_data_norm.shape, training_target.shape)
+print('validation data shape:', val_data_norm.shape, val_target.shape)
+print('testing data shape:', testing_data_norm.shape, testing_target.shape)
 
-        test_loss_list_tmp = []
-        for x, y in testing_dataloader:
-            output = net(x)
-            test_loss_list_tmp.append(loss_function(output, y).mean().asnumpy()[0])
+# model initialization
+backbones = [
+    {
+        'num_of_time_conv_filters1': 32,
+        'num_of_time_conv_filters2': 64,
+        'K_t': 3,
+        'num_of_cheb_filters': 32,
+        'K': 1,
+        'cheb_polys': cheb_polys
+    },
+    {
+        'num_of_time_conv_filters1': 32,
+        'num_of_time_conv_filters2': 128,
+        'K_t': 3,
+        'num_of_cheb_filters': 32,
+        'K': 1,
+        'cheb_polys': cheb_polys
+    }
+]
+net = STGCN(backbones, 128)
+net.initialize(ctx=ctx)
 
-        test_loss_list.append( sum(test_loss_list_tmp) / len(test_loss_list_tmp) )
+loss_function = gluon.loss.L2Loss()
 
-        print('current epoch is %s'%(epoch + 1))
-        print('training loss(MSE):', train_loss_list[-1])
-        print('validation loss(MSE):', val_loss_list[-1])
-        print('testing loss(MSE):', test_loss_list[-1])
-        print('time:', time.time() - t)
-        print()
+trainer = Trainer(net.collect_params(), optimizer)
+trainer.set_learning_rate(learning_rate)
+training_dataloader = gluon.data.DataLoader(
+    gluon.data.ArrayDataset(training_data_norm, training_target),
+    batch_size=batch_size, shuffle=False)
+validation_dataloader = gluon.data.DataLoader(
+    gluon.data.ArrayDataset(val_data_norm, val_target),
+    batch_size=batch_size, shuffle=False)
+testing_dataloader = gluon.data.DataLoader(
+    gluon.data.ArrayDataset(testing_data_norm, testing_target),
+    batch_size=batch_size, shuffle=False)
 
-        with open('results.log', 'a') as f:
-            f.write('training loss(MSE): %s'%(train_loss_list[-1]))
-            f.write('\n')
-            f.write('validation loss(MSE): %s'%(val_loss_list[-1]))
-            f.write('\n')
-            f.write('testing loss(MSE): %s'%(test_loss_list[-1]))
-            f.write('\n\n')
+if os.path.exists(args.logdir):
+    if args.rm:
+        shutil.rmtree(args.logdir)
+    else:
+        raise ValueError("log dir exists!")
 
-        if (epoch + 1) % 5 == 0:
-            filename = 'stgcn_params/stgcn.params_%s'%(epoch)
-            net.save_parameters(filename)
+if os.path.exists(params_dir):
+    if args.rm:
+        shutil.rmtree(params_dir)
+    else:
+        raise ValueError("params dir exists!")
 
-        if (epoch + 1) % decay_interval == 0:
-            trainer.set_learning_rate(trainer.learning_rate * decay_rate)
-    
-    return train_loss_list, val_loss_list, test_loss_list
+if not os.path.exists(params_dir):
+    os.mkdir(params_dir)
 
-if __name__ == "__main__":
-    A, X = data_preprocess('data/test_data1/distance.csv')
+sw = SummaryWriter(args.logdir, flush_secs=5)
 
-    L_tilde = scaled_Laplacian(A)
-    cheb_polys = [nd.array(i, ctx=ctx) for i in cheb_polynomial(L_tilde, orders_of_cheb)]
-
-    # training: validation: testing = 6: 2: 2
-    split_line1 = int(X.shape[2] * 0.6)
-    split_line2 = int(X.shape[2] * 0.8)
-
-    train_original_data = X[:, :, : split_line1]
-    val_original_data = X[:, :, split_line1: split_line2]
-    test_original_data = X[:, :, split_line2: ]
-
-    training_data, training_target = build_dataset(train_original_data)
-    val_data, val_target = build_dataset(val_original_data)
-    testing_data, testing_target = build_dataset(test_original_data)
-
-    # Z-score preprocessing
-    assert num_of_vertices == training_data.shape[2]
-    assert num_points_for_train == training_data.shape[3]
-    _, num_of_features, _, _ = training_data.shape
-
-    transformer = StandardScaler()
-    training_data_norm = transformer.fit_transform(training_data.reshape(training_data.shape[0], -1))\
-        .reshape(training_data.shape[0], num_of_features, num_of_vertices, num_points_for_train)
-    val_data_norm = transformer.transform(val_data.reshape(val_data.shape[0], -1))\
-        .reshape(val_data.shape[0], num_of_features, num_of_vertices, num_points_for_train)
-    testing_data_norm = transformer.transform(testing_data.reshape(testing_data.shape[0], -1))\
-        .reshape(testing_data.shape[0], num_of_features, num_of_vertices, num_points_for_train)
-
-    training_data_norm = nd.array(training_data_norm, ctx = ctx)
-    val_data_norm = nd.array(val_data_norm, ctx = ctx)
-    testing_data_norm = nd.array(testing_data_norm, ctx = ctx)
-
-    training_target = nd.array(training_target, ctx = ctx)
-    val_target = nd.array(val_target, ctx = ctx)
-    testing_target = nd.array(testing_target, ctx = ctx)
-
-    print('training data shape:', training_data_norm.shape, training_target.shape)
-    print('validation data shape:', val_data_norm.shape, val_target.shape)
-    print('testing data shape:', testing_data_norm.shape, testing_target.shape)
-
-    # model initialization
-    backbones = [
-        {
-            'num_of_time_conv_filters1': 32,
-            'num_of_time_conv_filters2': 64,
-            'K_t': 3,
-            'num_of_cheb_filters': 32,
-            'K': 1,
-            'cheb_polys': cheb_polys
-        },
-        {
-            'num_of_time_conv_filters1': 32,
-            'num_of_time_conv_filters2': 128,
-            'K_t': 3,
-            'num_of_cheb_filters': 32,
-            'K': 1,
-            'cheb_polys': cheb_polys
-        }
-    ]
-    net = STGCN(backbones, 128)
-    net.initialize(ctx=ctx)
-
-    loss_function = gluon.loss.L2Loss()
-
-    trainer = Trainer(net.collect_params(), optimizer, {'learning_rate': learning_rate})
-    training_dataloader = gluon.data.DataLoader(gluon.data.ArrayDataset(training_data_norm, training_target), batch_size=batch_size, shuffle=True)
-    validation_dataloader = gluon.data.DataLoader(gluon.data.ArrayDataset(val_data_norm, val_target), batch_size=batch_size, shuffle=False)
-    testing_dataloader = gluon.data.DataLoader(gluon.data.ArrayDataset(testing_data_norm, testing_target), batch_size=batch_size, shuffle=False)
-
-    if not os.path.exists('stgcn_params'):
-        os.mkdir('stgcn_params')
-
-    train_loss_list, val_loss_list, test_loss_list = train_model(net, training_dataloader, validation_dataloader, testing_dataloader)
+utils.train_model(net, sw, 0, 0,
+                  training_dataloader, validation_dataloader,
+                  testing_dataloader, epochs, loss_function,
+                  trainer, decay_interval, decay_rate)
